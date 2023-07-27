80d8ba5be5486b21873f24a73f2141a03feb4921
BahruDa <bahrudari68@gmail.com>
2023-07-27 12:40:41 +0300

    ES6_classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,21 +1,20 @@
-export default class Building {
-	  constructor(sqft) {
-		      this.sqft = sqft;
-		      if (this.constructor !== Building) {
-			            if (typeof this.evacuationWarningMessage !== 'function') {
-					            throw new Error(
-							              'Class extending Building must override evacuationWarningMessage',
-							            );
-					          }
-			          }
-		    }
+class Building {
+  constructor(sqft) {
+    if (
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
 
-	  get sqft() {
-		      return this._sqft;
-		    }
+    this._sqft = sqft;
+  }
 
-	  set sqft(value) {
-		      this._sqft = value;
-		    }
+  get sqft() {
+    return this._sqft;
+  }
 }
 
+export default Building;
--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,21 +1,22 @@
 import Building from './5-building';
 
-export default class SkyHighBuilding extends Building {
-	  constructor(sqft, floors) {
-		      super(sqft);
-		      this.floors = floors;
-		    }
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-	  get floors() {
-		      return this._floors;
-		    }
+  get sqft() {
+    return this._sqft;
+  }
 
-	  set floors(value) {
-		      this._floors = value;
-		    }
+  get floors() {
+    return this._floors;
+  }
 
-	  evacuationWarningMessage() {
-		      return `Evacuate slowly the ${this.floors} floors`;
-		    }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
 }
 
+export default SkyHighBuilding;
